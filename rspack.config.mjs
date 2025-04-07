a8e2078fe97ffabaf4c70c2b97ef11003b792100
Piotr Wieleba <wieleba.piotr@gmail.com>
2025-04-07 14:40:23 +0200

    initial commit

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -1,9 +1,13 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import {fileURLToPath} from 'node:url';
 import * as Repack from '@callstack/repack';
+import fs from 'fs';
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const STANDALONE = Boolean(process.env.STANDALONE);
 
 /**
  * Rspack configuration enhanced with Re.Pack defaults for React Native.
@@ -12,17 +16,50 @@ const __dirname = path.dirname(__filename);
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-export default {
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(),
-  },
-  module: {
-    rules: [
-      ...Repack.getJsTransformRules(),
-      ...Repack.getAssetTransformRules(),
+export default env => {
+  const {platform, mode} = env;
+
+  return {
+    mode,
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    // output: {
+    //   uniqueName: 'repack-child-app',
+    // },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules({inline: true}),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'ChildApp',
+        filename: 'ChildApp.container.js.bundle',
+        dts: false,
+        exposes: {
+          './App': './App.tsx',
+        },
+        shared: Object.fromEntries(
+          Object.entries(pkg.dependencies).map(([dep, {version}]) => {
+            return [
+              dep,
+              {singleton: true, eager: true, requiredVersion: version},
+            ];
+          }),
+        ),
+      }),
+      // Supports for new architecture - Hermes can also use JS, it's not a requirement,
+      // it will still work the same but it's for performance optimization
+      new Repack.plugins.HermesBytecodePlugin({
+        enabled: mode === 'production',
+        test: /\.(js)?bundle$/,
+        exclude: /index.bundle$/,
+      }),
     ],
-  },
-  plugins: [new Repack.RepackPlugin()],
+  };
 };
